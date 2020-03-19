98698f95b92ebf7d714d146ea9f22ec6ea4961c8
israolivares1987 <isra.olivares@gmail.com>
2020-03-19 16:52:17 -0300

    Update config.php

--- MIMS-Servicios/application/config/config.php
+++ MIMS-Servicios/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/git/MIMS-Intelligent-Projects/MIMS-Servicios/';
+$config['base_url'] = 'https://mimsprojects.com/MIMS-Intelligent-Projects/MIMS-Servicios/';
 
 /*
 |--------------------------------------------------------------------------
