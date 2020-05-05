f1747b72d5a872a99b6a096b24488edf2c6ae85c
Israel Olivares <israel.olivares@composer.cl>
2020-05-05 18:36:43 -0400

    se limpia master

--- MIMS-Front-End/application/views/activador/footer.php
+++
@@ -1,8 +0,0 @@
-     <!-- Main Footer -->
-  <footer class="main-footer">
-    <strong>Copyright &copy; 2020 <a href="http://mimsprojects.com">mimsprojects.com</a></strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.0.2
-    </div>
-  </footer>
